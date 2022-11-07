64ac6a6620087f17e71a2622ae88d4b759bb00ff
jiyangMin <mjeeyang@naver.com>
2022-11-07 09:30:59 +0900

    0922프로젝트

---
+++ src/j0922/Cal.java
@@ -0,0 +1,13 @@
+package j0922;
+
+public class Cal {
+
+	public static void main(String[] args) {
+	int num1=100;
+	int num2=200;
+	int result=num1+num2;
+	System.out.printf("%d+%d=%d\n",num1,num2,result);
+
+	}
+
+}
